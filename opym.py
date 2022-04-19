fb7095e0de4be848a2109a722979ba71b3db4bf2
George O'Neill <george.oneill.90@gmail.com>
2022-04-19 08:14:59 +0100

    init commit

---
+++ opym.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 13:29:53 2022
+
+@author: goneill
+"""
+
+import numpy as np
+from mne.io.constants import FIFF
+from mne.io.meas_info import _empty_info
+from mne.io.write import get_new_file_id
+
+def _get_plane_vectors(ez):
+    """Get two orthogonal vectors orthogonal to ez (ez will be modified)."""
+    assert ez.shape == (3,)
+    ez_len = np.sqrt(np.sum(ez * ez))
+    if ez_len == 0:
+        raise RuntimeError('Zero length normal. Cannot proceed.')
+    if np.abs(ez_len - np.abs(ez[2])) < 1e-5:  # ez already in z-direction
+        ex = np.array([1., 0., 0.])
+    else:
+        ex = np.zeros(3)
+        if ez[1] < ez[2]:
+            ex[0 if ez[0] < ez[1] else 1] = 1.
+        else:
+            ex[0 if ez[0] < ez[2] else 2] = 1.
+    ez /= ez_len
+    ex -= np.dot(ez, ex) * ez
+    ex /= np.sqrt(np.sum(ex * ex))
+    ey = np.cross(ez, ex)
+    return ex, ey
+
+def _convert_channel_info(chans):
+    nmeg = nstim = nmisc = nref = 0
+    chs = list()
+    for ii in range(0,len(chans['name'])):
+        ch = dict(scanno=ii + 1, range=1., cal=1., loc=np.full(12, np.nan),
+                  unit_mul=FIFF.FIFF_UNITM_NONE, ch_name=chans['name'][ii],
+                  coil_type=FIFF.FIFFV_COIL_NONE)    
+       
+        chs.append(ch)
+       
+        # create the channel information
+        if chans['pos'][ii] is not None:
+            r0 = chans['pos'][ii].copy()
+            ez = chans['ori'][ii].copy()
+            ex, ey = _get_plane_vectors(ez)
+            ch['loc'] = np.concatenate([r0, ex, ey, ez])
+            
+        if chans['type'][ii] == 'MEGMAG':
+            nmeg += 1
+            ch.update(logno=nmeg, coord_frame=FIFF.FIFFV_COORD_DEVICE,
+                      kind=FIFF.FIFFV_MEG_CH, unit=FIFF.FIFF_UNIT_T,
+                      coil_type=FIFF.FIFFV_COIL_QUSPIN_ZFOPM_MAG2)
+        elif chans['type'][ii] == 'MEGREFMAG':
+            nref += 1
+            ch.update(logno=nref, coord_frame=FIFF.FIFFV_COORD_UNKNOWN,
+                      kind=FIFF.FIFFV_REF_MEG_CH, unit=FIFF.FIFF_UNIT_V,
+                      coil_type=FIFF.FIFFV_COIL_QUSPIN_ZFOPM_MAG2)
+        elif chans['type'][ii] == 'TRIG':
+            nstim += 1
+            ch.update(logno=nstim, coord_frame=FIFF.FIFFV_COORD_UNKNOWN,
+                      kind=FIFF.FIFFV_STIM_CH, unit=FIFF.FIFF_UNIT_V)
+        else:
+            nmisc += 1
+            ch.update(logno=nmisc, coord_frame=FIFF.FIFFV_COORD_UNKNOWN,
+                      kind=FIFF.FIFFV_MISC_CH, unit=FIFF.FIFF_UNIT_NONE)
+    return chs
+
+def _compose_meas_info(meg,chans):
+    """Create info structure"""
+    info = _empty_info(meg['SamplingFrequency'])
+    
+    # Collect all the necessary data from the structures read
+    info['meas_id'] = get_new_file_id()
+    info['chs'] = _convert_channel_info(chans)
+    info['line_freq'] = meg['PowerLineFrequency']
+    info['bads'] = _read_bad_channels(chans)
+    info._unlocked = False
+    info._update_redundant()
+    return info
+
+def _read_bad_channels(chans):
+    bads = list()
+    for ii in range(0,len(chans['status'])):
+        if chans['status'][ii] == 'bad':
+            bads.append(chans['name'][ii])
+    return bads
+                    
--- test.py
+++ test.py
@@ -5,11 +5,22 @@ Created on Tue Apr 12 13:32:23 2022
 @author: goneill
 """
 
+from opym import _compose_meas_info
+import numpy as np
+import matplotlib.pyplot as plt
 import os.path as op
 import tsv_handler as tsv
 import json
 
-data_root = op.abspath('C:/Users/goneill/Downloads/masters_22/data');
+precision = 'single';
+
+match precision:
+    case 'single':
+        dt = '>f'
+    case 'double':
+        dt = '>d'
+
+data_root = op.abspath('C:/Users/goneill/Downloads/masters_22/data')
 bids_root = 'sub-001_ses-001_task-motor4way_run-001'
 
 files = dict();
@@ -19,3 +30,41 @@ files['chans'] = op.join(data_root,bids_root + '_channels.tsv')
 files['positions'] = op.join(data_root,bids_root + '_positions.tsv')
 files['coordsystem'] = op.join(data_root,bids_root + '_coordsystem.json')
 
+data = np.fromfile(files['bin'],dt)
+chans = tsv._from_tsv(files['chans'])
+chanpos = tsv._from_tsv(files['positions'])
+nchans = len(chans['name'])
+nlocs = len(chanpos['name'])
+nsamples = len(data)
+
+data.shape = (int(nsamples/nchans),nchans)
+
+# add some additional channel information
+# chans['type_mne'] = list()
+# for ii in range(0,nchans):
+#     match chans['type'][ii]:
+#         case 'MEGMAG':
+#             chans['type_mne'].append('mag')
+#         case 'MEGREFMAG':
+#             chans['type_mne'].append('ref_meg')
+#         case 'TRIG':
+#             chans['type_mne'].append('misc')
+#         case _:
+#             chans['type_mne'].append('misc')
+
+chans['pos'] = [None] * nchans
+chans['ori'] = [None] * nchans
+for ii in range(0,nlocs):
+    idx = chans['name'].index(chanpos['name'][ii])
+    tmp = np.array([chanpos['Px'][ii], chanpos['Py'][ii], chanpos['Pz'][ii]])
+    chans['pos'][idx] = tmp.astype(np.float64)/100
+    tmp = np.array([chanpos['Ox'][ii], chanpos['Oy'][ii], chanpos['Oz'][ii]])
+    chans['ori'][idx] = tmp.astype(np.float64)
+    
+fid = open(files['meg'],'r')
+meg = json.load(fid)
+#fid = open(files['coordsystem'],'r')
+#coord = json.load(fid)
+# chans = 
+
+info = _compose_meas_info(meg,chans)
